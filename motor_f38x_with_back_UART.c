c79c92416c50270a58174ce1fee374c21d7432c0
Carlos Castro <23carloscastro@gmail.com>
2017-04-03 17:55:51 -0700

    Code for backwards combined with UART 5:55pm

--- motor_f38x_with_back_UART.c
+++ motor_f38x_with_back_UART.c
@@ -14,7 +14,7 @@
 #define LEFT2 P2_6
 #define RIGHT1 P2_5
 #define RIGHT2 P2_4
-#define Comparator P2_3  //for detecting the square wave
+#define Comparator P0_1  //for detecting the square wave
 
 char buffer[33]; // for turning int into string for LCD
 volatile unsigned char pwm_count=0;
@@ -66,7 +66,8 @@ char _c51_external_startup (void)
 	P0MDOUT |= 0x10; // Enable UTX as push-pull output
 	XBR0     = 0x01; // Enable UART on P0.4(TX) and P0.5(RX)                     
 	XBR1     = 0x40; // Enable crossbar and weak pull-ups
-
+	XBR2     = 0x01;      // Enable UART1 on P0.0(TX1) and P0.1(RX1)
+	
 	// Initialize timer 2 for periodic interrupts
 	TMR2CN=0x00;   // Stop Timer2; Clear TF2;
 	CKCON|=0b_0001_0000;
@@ -85,7 +86,7 @@ void PORT_Init (void)
 	P0MDOUT |= 0x10; // Enable UART TX as push-pull output
 	XBR0=0b_0000_0001; // Enable UART on P0.4(TX) and P0.5(RX)                    
 	XBR1=0b_0101_0000; // Enable crossbar.  Enable T0 input.
-	XBR2=0b_0000_0000;
+	//XBR2=0b_0000_0000;
 }
 
 void SYSCLK_Init (void)
@@ -131,6 +132,47 @@ void UART0_Init (void)
 	TR1 = 1; // START Timer1
 	TI = 1;  // Indicate TX0 ready
 }
+
+void UART1_Init (unsigned long baudrate)
+{
+	SMOD1 = 0x0C; // no parity, 8 data bits, 1 stop bit
+	SCON1 = 0x10;
+
+	if (((SYSCLK/baudrate)/2L)/0xFFFFL < 1)
+	{
+		SBRL1 = 0x10000L-((SYSCLK/baudrate)/2L);
+		SBCON1 |= 0x03; // set prescaler to 1
+	}
+	else if (((SYSCLK/baudrate)/2L)/0xFFFFL < 4)
+	{
+		SBRL1 = 0x10000L-(((SYSCLK/baudrate)/2L)/4L);
+		SBCON1 &= ~0x03;
+		SBCON1 |= 0x01; // set prescaler to 4
+	}
+	else if (((SYSCLK/baudrate)/2)/0xFFFFL < 12)
+	{
+		SBRL1 = 0x10000L-(((SYSCLK/baudrate)/2L)/12L);
+		SBCON1 &= ~0x03; // set prescaler to 12
+	}
+	else
+	{
+		SBRL1 = 0x10000L-(((SYSCLK/baudrate)/2L)/48L);
+		SBCON1 &= ~0x03;
+		SBCON1 |= 0x02; // set prescaler to ?
+	}
+	SCON1 |= 0x02;    // indicate ready for TX
+	SBCON1 |= 0x40;   // enable baud rate generator
+}
+
+char getchar1 (void)
+{
+	char c;
+	while (!(SCON1 & 0x01));
+	SCON1 &= ~0x01;
+	SCON1&=0b_0011_1111;
+	c = SBUF1;
+	return (c);
+} 
 
  void TIMER0_Init(void)
 {
@@ -259,7 +301,7 @@ unsigned int turnrightflag=0;
 
 void main (void)
 {	
-
+	char c;
 	float x=0.0;
 	float deltaV;
 	volatile float V[4];
@@ -270,10 +312,14 @@ void main (void)
 //	float comparator_counter;
 	int count=0;
 	int start_timer_flag = 0;
-	int turn_180_flag = 1;
+	int turn_180_flag = 0;
+	int back_flag = 1;
+	int a = 0;
 
-	PORT_Init();     // Initialize Port I/O
+	//PORT_Init();     // Initialize Port I/O
 	SYSCLK_Init ();  // Initialize Oscillator
+	XBR2 |= 0b0000_0001; //pin 0.1
+	UART1_Init(50);
 	UART0_Init();    // Initialize UART0
 	TIMER0_Init();
 
@@ -282,7 +328,7 @@ void main (void)
 	InitPinADC(2, 0); // Configure P2.0 as analog input ---  middle inductor
 	InitPinADC(2, 1); // Configure P2.1 as analog input ---  right inductor
 	InitPinADC(2, 2); // Configure P2.2 as analog input ---  left inductor
-	
+	InitPinADC(1,7); // Configure P1.7 as analog input -- back inductor
 	TF0 = 0;
 	
 
@@ -295,8 +341,38 @@ void main (void)
 	    V[0]=Volts_at_Pin(LQFP32_MUX_P2_0); //middle inductor
    		V[1]=Volts_at_Pin(LQFP32_MUX_P2_2); //left inductor
         V[2]=Volts_at_Pin(LQFP32_MUX_P2_1); //right inductor
+        V[3]=Volts_at_Pin(LQFP32_MUX_P1_7); //back inductor
+		// UART1
+		c = getchar1();
+		a = (int) c;
+		checkinstruction(a);
+		printf("a = %d\n", c);
         
-        
+		
+		//printf("Vmiddle=%5.3f, Vleft=%5.3f, Vright=%5.3f, Vback= %5.3f, delta =%5.3f\r",V[0], V[1], V[2], V[3], deltaV);
+		//-------------Backwards CODE--------//
+		//---------------------------------//
+		if(V[3]<1){
+			if(V[0]>1.6){
+			pwm3=70;
+			pwm4=0;
+			pwm1=0;
+			pwm2=10;
+			waitms(50);
+			}
+			if(V[0]>0.5 && V[0]<1.6){//turn left
+			pwm1=70;
+			pwm2=0;
+			pwm3=0;
+			pwm4=10;
+			waitms(50);
+		
+			}
+		}
+		//-------------Backwards Code Done--------//
+		
+		//-------------STOP CODE--------//
+		//---------------------------------//
         if(stopflag==-1){
         pwm1=0;
         pwm2=0;
@@ -304,6 +380,8 @@ void main (void)
         pwm4=0;
         //start_timer_flag = 0;
         }
+		//-------------STOP CODE--------//
+		//---------------------------------//
        	
        
 		if(V[1] > V[2]){      //left wheel voltage > right wheel voltage
@@ -315,9 +393,7 @@ void main (void)
    		else{                 //right wheel voltage = left wheel voltage
    		deltaV = 0.0;
    		}
-
-   		//printf("Vmiddle=%5.3f, Vleft=%5.3f, Vright=%5.3f, delta =%5.3f\r\n",V[0], V[1], V[2], deltaV);
-        
+		
 		/*if(V[0] >= 2.2  && stopflag==0 && turnrightflag == 1 ){  //detecting intersection, usually V[0] = 0, 
 				pwm1= 0;
 	            pwm2= 50;
@@ -392,7 +468,6 @@ void main (void)
 			}
 		*/
 		
-	//	turn_180();
 	
 		// turn 180
 		//Left wheel turns forward, right wheel turns backward
@@ -428,14 +503,14 @@ void main (void)
 		// pwm3 right wheel motor for pin 2.5
 		// pwm4 right wheel motor for pin 2.4	
         
-        if(V[2]/V[1] == 1 && stopflag==0){ //when right wheel = left wheel
+        if(V[2]/V[1] == 1 && stopflag==0 && back_flag==0){ //when right wheel = left wheel
         	pwm1 = 0;  
         	pwm2 = 100;  
         	pwm3 = 0;  
         	pwm4 = 100;
         } 
         	
-        if(V[1]>V[2]  && stopflag==0){ //right wheel is closer than left wheel, decrease right pwm Vleft>Vright
+        if(V[1]>V[2]  && stopflag==0 && back_flag==0){ //right wheel is closer than left wheel, decrease right pwm Vleft>Vright
         	pwm1 = 0;    // set left wheel at highest speed
         	pwm2 = 100; 
         	if(deltaV >= 0.5){
@@ -462,7 +537,7 @@ void main (void)
         //	pwm3 = abs(40.0 - x); 
     	}
         	
-        if(V[1]<V[2]  && stopflag==0){ //left wheel is closer, decrease left pwm.... Vleft<Vright
+        if(V[1]<V[2]  && stopflag==0  && back_flag==0){ //left wheel is closer, decrease left pwm.... Vleft<Vright
         	pwm3 = 0;  //set right wheel at max speed
         	pwm4 = 100;
         	if(deltaV >= 0.5){
@@ -511,7 +586,7 @@ void main (void)
 			Period=(count*65536.0+TH0*256.0+TL0);
 			printf("period = %5.3f\r\n", Period);
 			checkinstruction(Period);
-			Period = 0.0;
+		//	Period = 0.0;
 			flagcomp = 0;
 		}
 		
@@ -554,6 +629,162 @@ void main (void)
 				waitms(1200);
 				turn_180_flag=0;
 		}
+		
+		// Backwards
+		
+		/*
+		pwm1 "right" wheel
+ 		pwm2 "right" wheel 
+		pwm3 "left" wheel 
+		pwm4 "left" wheel 
+		
+		pwm 1 & 3 = "forward"
+		pwm 2 & 4 = "backward"
+		
+		V[0]middle inductor
+   		V[1]"right" inductor 
+      	V[2]"left" inductor
+		*/
+		
+		printf("Vmiddle=%5.3f, Vright=%5.3f, Vleft=%5.3f, delta =%5.3f\r",V[0], V[1], V[2], deltaV);
+		 
+		// Go "forward"
+		/*
+		 if(V[0]>1.5){
+			pwm1=0;
+			pwm2=0;
+			pwm3=40;
+			pwm4=0;
+			printf("1\n");
+		 }
+		 */
+		 // if middle inductor voltage is close to zero, move forward
+		if(V[0]<0.6){
+			pwm1=40;
+			pwm2=0;
+			pwm3=40;
+			pwm4=0;
+			printf("forward\n");
+			}
+			
+		// adjusts car position until the middle inductor value is close to zero	
+		 if(V[0]>0.6 && V[0]<1.9){
+			 
+			printf("adjustment"); 
+			 
+			// adjust, turn to left
+			// right inductor > left inductor
+			if (V[1] > V[2]) {
+				pwm1=20;
+				pwm2=0;
+				pwm3=0;
+				pwm4=0;
+				waitms(100);
+				pwm1 = 0;
+				pwm2 = 0;
+				pwm3 = 5;
+				pwm4 = 0;
+			}
+			// adjust, turn right
+			// right inductor < left inductor
+			if (V[1] < V[2]) {
+				pwm1 = 0;
+				pwm2 = 0;
+				pwm3 = 20;
+				pwm4 = 0;
+				waitms(100);
+				pwm1 = 5;
+				pwm2 = 0;
+				pwm3 = 0;
+				pwm4 = 0;
+				
+			}
+				
+		 }	
+		// turn 
+		if (V[0] > 1.9) && (deltaV > 1.0 ) {
+			printf("turning");
+			 //turn left
+			 if (V[1] > V[2]) {
+				 pwm1 = 20;
+				 pwm2 = 0;
+				 pwm3 = 0;
+				 pwm4 = 0;
+			 }
+			 // turn right
+			 if (V[1] < V[2]) {
+				pwm1 = 0;
+				pwm2 = 0;
+				pwm3 = 20;
+				pwm4 = 0;		 
+			}
+		}
+		
+		/*if(V[2]/V[1] == 1 && stopflag==0  && back_flag==1){ //when right wheel = left wheel
+        	pwm1 = 40; 
+        	pwm2 = 0;  
+       		pwm3 = 40;
+        	pwm4 = 0;  
+       	 } 
+        	
+        // V"right">V"left"
+        // Turn left	
+        if(V[1]>V[2]  && stopflag==0 && back_flag==1){ //right wheel is closer than left wheel, decrease right pwm Vleft>Vright
+        	pwm1 = 40; 
+        	pwm2 = 0;    // set right wheel speed
+        	if(deltaV >= 0.5){
+        		x = 0.0;
+        		pwm3 = 0.0;
+        		pwm4 = 40.0 - x;
+        	}   
+        	if(deltaV >= 0.4 && deltaV < 0.5){
+        		x = 100.0*(deltaV/0.6);
+        		pwm3 = 0.0;
+        		pwm4 = 40.0 - x;
+       
+        	}
+        	if(deltaV < 0.4 && deltaV > 0.1) {
+        		x = (100.0*(deltaV/0.6))/1.5; 
+        		pwm3 = 40.0 - x;
+        		pwm4 = 0.0;
+        	}
+        	if(deltaV <= 0.1){
+        		x = 0.0;
+        		pwm3 = 40.0 - x;
+        		pwm4 = 0.0;
+        	}
+         
+    	}
+        
+    	// V"right"<V"left"
+    	//Turn Right
+        if(V[1]<V[2]  && stopflag==0 && back_flag==1){ 
+        	pwm3 = 40;
+        	pwm4 = 0;  //set "left" wheel at max speed
+        	// turn "right"
+        	if(deltaV >= 0.5){
+        		//x = 0.0;
+        		pwm1 = 0;
+        		pwm2 = 40;
+        	}
+        	// adjust "right" wheel speed to correct car
+        	if(deltaV >= 0.3 && deltaV < 0.5){
+        		x = 100.0*(deltaV/0.6);
+        		pwm1 = 0;
+        		pwm2 = 40 - x;
+        	}
+        	if(deltaV < 0.3 && deltaV > 0.1) {
+        		x = (100.0*(deltaV/0.6))/1.5;
+        		pwm1 = 40 - x;
+        		pwm2 = 0;
+        	}
+        	// go "forward"
+        	if(deltaV <= 0.1){
+        		//x = 0.0;
+        		pwm1 = 40;
+        		pwm2 = 0;
+        	}
+        }*/
        
     }
    
